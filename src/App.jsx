ac02659ad5c165be14c0e66dea605e01dc87c4a7
mwebaze-nicho <nickmwebaze@gmail.com>
2024-02-26 19:09:45 +0300

    *

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,6 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
 
-//checking
-
 const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
